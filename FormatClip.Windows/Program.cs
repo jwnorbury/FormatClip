0e33c1652e4f98e3666d9f139c89ab63b6ae3c75
James Norbury <jwnorbury@gmail.com>
2020-07-05 18:20:46 +0100

    Add Windows specific implementation to copy result to clipboard if formatting occured.

---
+++ FormatClip.Windows/Program.cs
@@ -0,0 +1,32 @@
+﻿using FormatClip.Core;
+using System;
+using System.Diagnostics;
+using System.Windows;
+
+namespace FormatClip.Windows
+{
+    class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            var input = GetInput(args);
+            var writer = FormatterFactory.Create(input);
+            var result = writer.Format();
+            if (!input.Equals(result))
+            {
+                Clipboard.SetText(result);
+            }
+        }
+
+        private static string GetInput(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                return args[0];
+            }
+
+            return Clipboard.GetText();
+        }
+    }
+}
